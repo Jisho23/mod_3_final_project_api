002b0f54badb24b2398627b7db8df2ab7f0cc62f
mattcrowe5 <crowe.matthew5@gmail.com>
2017-11-14 10:37:41 -0500

    added pterodactyl

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,4 @@ es << fireball
 wizard << fireball
 goblin = Monster.find_or_create_by(name: 'goblin', hp: 6, attack: 1, exp: 3)
 dragon = Monster.find_or_create_by(name: 'dragon', hp: 15, attack: 4, exp: 10)
+pterodactyl = Monster.find_or_create_by(name: 'pterodactyl', hp: 10, attack: 3, exp: 7)
